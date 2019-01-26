40f8963056b336212027ff13632bb7c888f7b94b
frank <frankam@inbox.lv>
2019-01-26 09:11:19 +0100

    wolke wechselt sprite

---
+++ Assets/Scripts/WolkeGesichtAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WolkeGesichtAnimation : MonoBehaviour
+{
+    private IEnumerator coroutine;
+    public Texture[] textures;
+    private int counter;
+    private Renderer renderer;
+
+    void Start()
+    {
+        coroutine = ChangeSprite(2.0f);
+        counter = 0;
+        StartCoroutine(coroutine);
+        renderer = GetComponent<Renderer>();
+    }
+
+    private IEnumerator ChangeSprite(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            //print("WaitAndPrint " + Time.time);
+            renderer.material.mainTexture = textures[counter % textures.Length ];
+            counter++;
+        }
+    }
+}
