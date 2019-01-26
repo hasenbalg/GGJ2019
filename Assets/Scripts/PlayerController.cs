d5212e72871f8543f868b75327a3b6da32284f74
frank <frankam@inbox.lv>
2019-01-26 11:37:33 +0100

    wiese und neue steuereung

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,8 +9,8 @@ public class PlayerController : MonoBehaviour
 	public float speed = 6.0f;
 	public float jumpSpeed = 8.0f;
 	public float gravity = 20.0f;
-
-	public float rotSpeed = 2f;
+    [Tooltip("Grad pro Sekunde")]
+    public float rotSpeed = 300.0f;
 
 	private Vector3 moveDirection = Vector3.zero;
 	private CharacterController controller;
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
 			// We are grounded, so recalculate
 			// move direction directly from axes
 
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+			moveDirection = new Vector3(0.0f , 0.0f, Input.GetAxis("Vertical"));
 			moveDirection = transform.TransformDirection(moveDirection);
 			moveDirection = moveDirection * speed;
 
@@ -45,5 +45,10 @@ public class PlayerController : MonoBehaviour
 
 		// Move the controller
 		controller.Move(moveDirection * Time.deltaTime);
-	}
+
+
+        Quaternion destRotation = controller.transform.rotation;
+        destRotation.eulerAngles = destRotation.eulerAngles + new Vector3(0, Input.GetAxis("Horizontal") * 2, 0);
+        controller.transform.rotation = Quaternion.RotateTowards(transform.rotation, destRotation, Time.deltaTime * rotSpeed);
+    }
 }
